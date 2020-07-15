eba0d524b47d1e6bbf04618ba37d6b34d025c923
alexandercrisler <alexcrisler@gmail.com>
2020-07-15 08:42:03 -0500

    added movement to the aliens to the right

--- alien_invasion.py
+++ alien_invasion.py
@@ -35,6 +35,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
 
@@ -87,6 +88,11 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
+    
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet."""
+        self.aliens.update()
 
     
     def _create_fleet(self):
